884d922e2da23c22e5b98c124c674d7b83a43be6
Javier Fernández Mayo <jotamayo97@gmail.com>
2025-11-05 17:29:35 +0100

    Initial commit: empty solution with main and test projects

---
+++ Katalyst.Tests/GameShould.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+using System;
+using Assert = Xunit.Assert;
+
+namespace Katalyst.Tests;
+
+public class GameShould
+{
+    [Fact]
+    public void NewGame_ShouldStartWithEmptyBoard()
+    {
+        var game = new Game();
+
+        var board = game.GetBoard();
+
+        Assert.All(board, cell => Assert.Null(cell));
+    }
+    
+}
---
+++ Katalyst/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace Katalyst;
+
+public class Game
+{
+    private readonly string?[] _board = new string?[9];
+
+    public string?[] GetBoard() => _board;
+}
